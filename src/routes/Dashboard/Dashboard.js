ea79a48e4abea7eb3b058d40f1122a0ec214e67c
Michaelhyunookoh <Michaelhyunookoh@gmail.com>
2020-10-08 00:53:26 -0700

    refactor

--- src/routes/Dashboard/Dashboard.js
+++ src/routes/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Subscription from "../../components/Subscription/Subscription";
 import config from "../../config";
@@ -6,12 +6,11 @@ import TokenService from "../../services/token-service";
 import Header from "../../components/Header/Header";
 import "./Dashboard.css";
 
-export default class Dashboard extends React.Component {
-  state = {
-    subscriptions: [],
-  };
+export default function Dashboard(props) {
+  const [subscriptions, setSubscriptions] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
+    let isMounted = true;
     Promise.all([
       fetch(`${config.API_ENDPOINT}/api/subscriptions`, {
         method: "GET",
@@ -24,50 +23,48 @@ export default class Dashboard extends React.Component {
         return Promise.all([subscriptionsRes.json()]);
       })
       .then(([subscriptions]) => {
-        this.setState({
-          subscriptions,
-        });
+        if (isMounted) setSubscriptions(subscriptions);
       })
       .catch((error) => {
         console.log({ error });
       });
-  }
-
-  deleteSubscription = (subscriptionId) => {
-    this.setState({
-      subscriptions: this.state.subscriptions.filter(
+    return () => {
+      isMounted = false;
+    };
+  });
+  const deleteSubscription = (subscriptionId) => {
+    setSubscriptions({
+      subscriptions: subscriptions.filter(
         (subscription) => subscription.id !== subscriptionId
       ),
     });
   };
 
-  render() {
-    return (
-      <section className="DashboardMain">
-        <header className="App-header">
-          <Header />
-        </header>
-        <section className="add-new-subscription">
-          <Link to="/addSubscription">
-            <button>Add New Subscription</button>
-          </Link>
-        </section>
-        <ul>
-          {this.state.subscriptions.map((subscription) => (
-            <li key={subscription.id}>
-              <Subscription
-                id={subscription.id}
-                name={subscription.subscription_name}
-                price={subscription.subscription_price}
-                username={subscription.subscription_user_name}
-                password={subscription.subscription_password}
-                category={subscription.category}
-                deleteSubscription={this.deleteSubscription}
-              />
-            </li>
-          ))}
-        </ul>
+  return (
+    <section className="DashboardMain">
+      <header className="App-header">
+        <Header />
+      </header>
+      <section className="add-new-subscription">
+        <Link to="/addSubscription">
+          <button>Add New Subscription</button>
+        </Link>
       </section>
-    );
-  }
+      <ul>
+        {subscriptions.map((subscription) => (
+          <li key={subscription.id}>
+            <Subscription
+              id={subscription.id}
+              name={subscription.subscription_name}
+              price={subscription.subscription_price}
+              username={subscription.subscription_user_name}
+              password={subscription.subscription_password}
+              category={subscription.category}
+              deleteSubscription={deleteSubscription}
+            />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
