65e2cced14f4796a7cec76b95451023b1fbd5b92
Michaelhyunookoh <Michaelhyunookoh@gmail.com>
2020-10-13 18:15:59 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -8,58 +8,72 @@ import PublicOnlyRoute from "./components/Utils/PublicOnlyRoute";
 import LandingPage from "./routes/LandingPage/LandingPage";
 import RegistrationForm from "./routes/RegistrationForm/RegistrationForm";
 
-function App(props) {
-  const [subscriptions, setSubscriptions] = useState([]);
-  const [hasError, setHasError] = useState(false);
-
-  useEffect((error) => {
-    console.error(error);
-    if (error) {
-      setHasError(true);
-    }
-  }, []);
-
-  const handleAddSubscription = (subscription) => {
-    setSubscriptions([subscription]);
+class App extends React.Component {
+  state = {
+    subscriptions: [],
+    hasError: false,
   };
 
-  const handleUpdateSubscription = (updatedSubscription) => {
-    const newSubscriptions = subscriptions.map((sub) =>
+  static getDerivedStateFromError(error) {
+    console.error(error);
+    return { hasError: true };
+  }
+
+  handleAddSubscription = (subscription) => {
+    this.setState({
+      subscriptions: [...this.state.subscriptions, subscription],
+    });
+  };
+
+  handleUpdateSubscription = (updatedSubscription) => {
+    const newSubscriptions = this.state.subscriptions.map((sub) =>
       sub.id === updatedSubscription.id ? updatedSubscription : sub
     );
-    setSubscriptions([newSubscriptions]);
+    this.setState({
+      subscriptions: newSubscriptions,
+    });
   };
 
-  return (
-    <main className="App">
-      <section className="app-with-header">
-        {hasError && <p className="red">There was an error! Oh no!</p>}
+  render() {
+    return (
+      <main className="App">
+        <section className="app-with-header">
+          {this.state.hasError && (
+            <p className="red">There was an error! Oh no!</p>
+          )}
 
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/dashboard" component={Dashboard} />
+
+          <Route
+            exact
+            path="/addSubscription"
+            render={() => (
+              <AddSubscription addSubscription={this.handleAddSubscription} />
+            )}
+          />
+        </section>
 
         <Route
           exact
-          path="/addSubscription"
+          path="/edit/:subscriptionId"
           render={() => (
-            <AddSubscription addSubscription={handleAddSubscription} />
+            <EditSubscription
+              updateSubscription={this.handleUpdateSubscription}
+            />
           )}
         />
-      </section>
 
-      <Route
-        exact
-        path="/edit/:subscriptionId"
-        render={() => (
-          <EditSubscription updateSubscription={handleUpdateSubscription} />
-        )}
-      />
+        <PublicOnlyRoute exact path={"/login"} component={LoginForm} />
 
-      <PublicOnlyRoute exact path={"/login"} component={LoginForm} />
-
-      <PublicOnlyRoute exact path={"/register"} component={RegistrationForm} />
-    </main>
-  );
+        <PublicOnlyRoute
+          exact
+          path={"/register"}
+          component={RegistrationForm}
+        />
+      </main>
+    );
+  }
 }
 
 export default App;
--- src/routes/LoginForm/LoginForm.js
+++ src/routes/LoginForm/LoginForm.js
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import { withRouter, Link, useHistory } from "react-router-dom";
 import TokenService from "../../services/token-service";
 import AuthService from "../../services/auth-service";
 import "./LoginForm.css";
 
-function LoginForm(props) {
-  const [error, setError] = useState(null);
-  const { push } = useHistory();
+class LoginForm extends Component {
+  state = {
+    error: null,
+  };
 
-  const handleSubmitJwtAuth = (ev) => {
+  handleSubmitJwtAuth = (ev) => {
     ev.preventDefault();
-    setError(null);
+    this.setState({ error: null });
     const { user_name, password } = ev.target;
 
     AuthService.postLogin({
@@ -21,36 +22,44 @@ function LoginForm(props) {
         user_name.value = "";
         password.value = "";
         TokenService.saveAuthToken(res.authToken);
-        push("/dashboard");
+        this.props.history.push("/dashboard");
       })
       .catch((error) => {
-        setError(error);
+        this.setState({ error });
         console.log(error);
       });
   };
 
-  return (
-    <section className="LoginPage">
-      <h2>Login</h2>
-      <form className="LoginForm" onSubmit={handleSubmitJwtAuth}>
-        <div role="alert">{error && <p className="red">{error.error}</p>}</div>
-        <div className="Login_name">
-          <label htmlFor="Login__user_name">User Name</label>
-          <input required type="text" id="user_name" name="user_name"></input>
-        </div>
-        <div className="Login_password">
-          <label htmlFor="Login__password">Password</label>
-          <input required type="password" id="password" name="password"></input>
-        </div>
-        <button type="submit" className="LoginPageButton">
-          Login
-        </button>
-        <Link to="/">
-          <button className="goBackButton">Go Back</button>
-        </Link>
-      </form>
-    </section>
-  );
+  render() {
+    const { error } = this.state;
+    return (
+      <section>
+        <form className="LoginForm" onSubmit={this.handleSubmitJwtAuth}>
+          <div role="alert">
+            {error && <p className="red">{error.error}</p>}
+          </div>
+          <div className="Login_name">
+            <label htmlFor="Login__user_name">User Name</label>
+            <input required type="text" id="user_name" name="user_name"></input>
+          </div>
+          <div className="Login_password">
+            <label htmlFor="Login__password">Password</label>
+            <input
+              required
+              type="password"
+              id="password"
+              name="password"
+            ></input>
+          </div>
+          <button type="submit" className="LoginPageButton">
+            Login
+          </button>
+          <Link to="/">
+            <button className="goBackButton">Go Back</button>
+          </Link>
+        </form>
+      </section>
+    );
+  }
 }
-
 export default withRouter(LoginForm);
