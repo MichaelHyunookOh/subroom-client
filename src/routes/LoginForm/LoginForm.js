4f4de292e0db298092a4baa77dbeaf29d76a4269
Michaelhyunookoh <Michaelhyunookoh@gmail.com>
2020-10-13 18:24:59 -0700

    changes

--- src/routes/LoginForm/LoginForm.js
+++ src/routes/LoginForm/LoginForm.js
@@ -1,17 +1,16 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { withRouter, Link, useHistory } from "react-router-dom";
 import TokenService from "../../services/token-service";
 import AuthService from "../../services/auth-service";
 import "./LoginForm.css";
 
-class LoginForm extends Component {
-  state = {
-    error: null,
-  };
+function LoginForm(props) {
+  const [error, setError] = useState(null);
+  const { push } = useHistory();
 
-  handleSubmitJwtAuth = (ev) => {
+  const handleSubmitJwtAuth = (ev) => {
     ev.preventDefault();
-    this.setState({ error: null });
+    setError(null);
     const { user_name, password } = ev.target;
 
     AuthService.postLogin({
@@ -22,44 +21,36 @@ class LoginForm extends Component {
         user_name.value = "";
         password.value = "";
         TokenService.saveAuthToken(res.authToken);
-        this.props.history.push("/dashboard");
+        push("/dashboard");
       })
       .catch((error) => {
-        this.setState({ error });
+        setError(error);
         console.log(error);
       });
   };
 
-  render() {
-    const { error } = this.state;
-    return (
-      <section>
-        <form className="LoginForm" onSubmit={this.handleSubmitJwtAuth}>
-          <div role="alert">
-            {error && <p className="red">{error.error}</p>}
-          </div>
-          <div className="Login_name">
-            <label htmlFor="Login__user_name">User Name</label>
-            <input required type="text" id="user_name" name="user_name"></input>
-          </div>
-          <div className="Login_password">
-            <label htmlFor="Login__password">Password</label>
-            <input
-              required
-              type="password"
-              id="password"
-              name="password"
-            ></input>
-          </div>
-          <button type="submit" className="LoginPageButton">
-            Login
-          </button>
-          <Link to="/">
-            <button className="goBackButton">Go Back</button>
-          </Link>
-        </form>
-      </section>
-    );
-  }
+  return (
+    <section className="LoginPage">
+      <h2>Login</h2>
+      <form className="LoginForm" onSubmit={handleSubmitJwtAuth}>
+        <div role="alert">{error && <p className="red">{error.error}</p>}</div>
+        <div className="Login_name">
+          <label htmlFor="Login__user_name">User Name</label>
+          <input required type="text" id="user_name" name="user_name"></input>
+        </div>
+        <div className="Login_password">
+          <label htmlFor="Login__password">Password</label>
+          <input required type="password" id="password" name="password"></input>
+        </div>
+        <button type="submit" className="LoginPageButton">
+          Login
+        </button>
+        <Link to="/">
+          <button className="goBackButton">Go Back</button>
+        </Link>
+      </form>
+    </section>
+  );
 }
+
 export default withRouter(LoginForm);
