2c1159266f7e351f20963926b5ed2ee747488c09
Michaelhyunookoh <Michaelhyunookoh@gmail.com>
2020-10-20 16:22:35 -0700

    login

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import TokenService from "../../services/token-service";
 
 import "./LandingPage.css";
 
 export default function LandingPage() {
-  return (
-    <main className="landing-page-main">
-      <header className="App-header">
-        <h1>Welcome to Subroom!</h1>
-        <h2>Your subscriptions need a home too</h2>
-      </header>
+  const login = () => {
+    return (
       <section className="buttons">
         <div className="login">
           <Link to="/login">
@@ -22,9 +19,31 @@ export default function LandingPage() {
           </Link>
         </div>
       </section>
+    );
+  };
+
+  const notLoggedIn = () => {
+    return (
+      <section className="buttons">
+        <div className="login">
+          <Link to="/dashboard">
+            <button>Dashboard</button>
+          </Link>
+        </div>
+      </section>
+    );
+  };
+
+  return (
+    <main className="landing-page-main">
+      <header className="App-header">
+        <h1>Welcome to Subroom!</h1>
+        <h2>Your subscriptions need a home too</h2>
+      </header>
+      {TokenService.hasAuthToken() ? notLoggedIn() : login()}
       <section className="description">
         <p>
-          {" "}
+          {""}
           Your online services are important, and we know that you care about
           them. We also know that you care about keeping track of what you are
           paying for (hopefully). Subroom is designed to organize all of your
